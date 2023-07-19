aa45f52a71c0658748e275fe7296643dce6125f2
Taisacra <tainasacramento2002@gmail.com>
2023-07-18 21:04:59 -0300

    atual

--- Exercicios/Exercicio014/script.js
+++ Exercicios/Exercicio014/script.js
@@ -1,1 +1,22 @@
-alert('Olá')
+function carregar(){
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('img')
+    var data = new Date()
+    var hora = data.getHours()
+    
+    msg.innerHTML = `Agora são ${hora} horas`
+
+    if (hora >= 0 && hora <12) {
+        //boa tarde
+        img.src = 'manha.png'
+        document.body.style.background = '#ffd699'
+    }else if (hora >= 12 && hora <18) {
+        //boa tarde
+        img.src = 'tarde.png'
+        document.body.style.background = '#b36b00'
+    }else{
+        //boa noite
+        img.src = 'noite.png'
+        document.body.style.background = '#00334d'
+    }
+}
