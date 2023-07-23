e6826a3bc9a52e4510efdfe3080cb52c5e91f603
Taisacra <tainasacramento2002@gmail.com>
2023-07-22 21:21:49 -0300

    atualização

--- Exercicios/Exercicio016/script.js
+++ Exercicios/Exercicio016/script.js
@@ -1,1 +1,35 @@
-alert('Olá')
+function Contar() {
+    var ini = window.document.getElementById('txtInicio')
+    var fim = window.document.getElementById('txtFim')
+    var ps = window.document.getElementById('txtPasso')
+    var res = window.document.getElementById('res')
+
+    
+    if (ini.value.length == 0 || fim.value.length == 0 || ps.value.length == 0 ) {
+        res.innerHTML ='Impossível contar!'
+        window.alert('[ERRO] Falta dados!')
+        
+    }else{
+        res.innerHTML = 'Contando: <br> '
+        let i = Number(ini.value) // converte a caixa de texto em numero
+        let f = Number(fim.value)
+        let p = Number(ps.value)
+        if (p <= 0) {
+            window.alert('Passo inválido! Considerando passo 1')
+            p = 1
+        }
+       
+        if (i < f) { //Contagem crescente
+            for (let c = i; c <= f ; c += p) {
+                res.innerHTML += ` ${c} \u{1F449} `
+            
+            }
+            res.innerHTML += `\u{1F3C1}!`
+            }else{//Contegem descrecente 
+                for (let c = i; c >= f ; c -= p) {
+                    res.innerHTML += ` ${c} \u{1F449} `
+                }
+                res.innerHTML += `\u{1F3C1}!`
+            }        
+        }
+}
